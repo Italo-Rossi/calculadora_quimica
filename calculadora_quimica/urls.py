0e7d39ac797d24da7c83e6b514336e1b2208dd79
Italo-Rossi <italorr@outlook.com>
2019-11-15 22:37:52 -0300

    Teste

--- calculadora_quimica/urls.py
+++ calculadora_quimica/urls.py
@@ -10,10 +10,11 @@ from django.urls import path
 from home.views import home as pag_inicial
 from about.views import about as sobre_nos
 from home.views import concentracao_molar as concentracao_molar
-
+#from home.views import contact as contato
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', pag_inicial, name='home_page'), #LINK PARA A ABA HOME OBS: Posteriormente podemos organizar melhor a disposição de cada função
-    path('sobre_nos', sobre_nos, name='sobre_nos' ),
+    path('', pag_inicial, name='home_page'), #roteamento PARA A ABA HOME OBS
+    path('sobre_nos', sobre_nos, name='sobre_nos' ), 
     path('concentracao_molar', concentracao_molar, name='concentracao_molar'),
+   # path('contato', contato, name='aba_para_contato'),
 ]
