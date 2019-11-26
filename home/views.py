640e56bd6475b50c54215813e4d35d8eaf421595
Italo-Rossi <italorr@outlook.com>
2019-11-26 03:42:43 -0300

    Alterações no template 'home.html' e na na views da app home

--- home/views.py
+++ home/views.py
@@ -3,38 +3,8 @@ from django.shortcuts import render
 from .forms import NameForm
 from .forms import CountryForm
 #context = {'NameForm':NameForm}
-def soma(a,b):
-    return (a+b)
 
 def home(request):
-    print(request.method)
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        
-        
-        form = NameForm(request.POST)   #Dúvida: o objeto NameForme aceita um dicionário como parâmetro? 
-        #form_2 = CountryForm(request.POST)
-        # check whether it's valid:
-        valor_1 = int(request.POST['valor_1'])
-        valor_2 = int(request.POST['valor_2'])
-
-        if form.is_valid():
-            
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            soma = sum([valor_1,valor_2])
-
-            contexto = {
-                'form':form
-            }
-            return render (request, "resultado.html", contexto)
-            
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-        #form_2 = CountryForm()
-
     return render(request, 'home.html', {'form': form})
 
 def concentracao_molar(request):
