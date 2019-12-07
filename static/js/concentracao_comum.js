454d31bc238f9d4d728f544108fa4ff18ab4d648
Italo-Rossi <italorr@outlook.com>
2019-12-07 03:08:55 -0300

    Alteração de front-end no bloco 'content' e ajuste na conversão do calculo de concentração comum

--- static/js/concentracao_comum.js
+++ static/js/concentracao_comum.js
@@ -1,27 +1,30 @@
  function concentracaoComum() {
-            validacao = false;
-            valor_a = document.getElementById('valor_1').value;
-            valor_b = document.getElementById('valor_2').value;
-            unidade_massa = document.getElementById('unidade_massa').value;
-            unidade_volume = document.getElementById('unidade_volume').value;
-            unidades = unidade_massa + '/' + unidade_volume;
-            
-            if (valor_a.indexOf(",") > -1 || valor_b.indexOf(",") > -1 ) {
-                alert("Para casas decimais, utilize ponto")
-            }
-            else {
-                valor_a = parseFloat(valor_a);
-                valor_b = parseFloat(valor_b);   
-            }
-            if ( (valor_b > 0) == false || (valor_a > 0)==false ) {  
-            //Verificação básica, para ser aprimorada
-                alert('Digite apenas valores maiores que zero');  
+        validacao = false;
+        valor_a = document.getElementById('valor_1').value;
+        valor_b = document.getElementById('valor_2').value;
+        unidade_massa = document.getElementById('unidade_massa').value;
+        unidade_volume = document.getElementById('unidade_volume').value;
+        unidades = unidade_massa + '/L'; //a unidade de volume na resposta deve estar sempre em litro;
+        
+        if (valor_a.indexOf(",") > -1 || valor_b.indexOf(",") > -1 ) { //Verifica se há vírgula em algum dos valores inseridos
+            alert("Para casas decimais, utilize ponto")
+        }
+        else {
+            valor_a = parseFloat(valor_a);
+            valor_b = parseFloat(valor_b); 
+            if ( (valor_b > 0) == false || (valor_a > 0)==false ) {  //Verifica se todos valores são maiores que 0
+            alert('Digite apenas valores maiores que zero');  
             }
             else {
                 validacao = true;
-            }
-            if (validacao == true) {
-                resultado = valor_a / valor_b;
-                document.getElementById('resultado').innerHTML = (resultado.toFixed(2)) + ' ' +unidades;
-            }
+            }  
+        }
+        
+        if (unidade_volume == 'ml'){ // Se a unidade for ml, o volumen é dividido por 1000
+            valor_b = (valor_b / 1000)
+        }
+        if (validacao == true) {
+            resultado = valor_a / valor_b;
+            document.getElementById('resultado').innerHTML = (resultado.toFixed(2)) + ' ' +unidades;
+        }
 }
